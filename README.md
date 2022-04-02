2cc9826e37078780e2aaeb16b3650b6fb6a14a82
Josh Klar <josh@klar.sh>
2022-04-02 01:40:50 -0700

    multiplication word ALMOST implemented; basic zig interpreter structure started; segfaults and leaks o plenty

--- README.md
+++ README.md
@@ -9,21 +9,9 @@
        _|                 /           _|                   _|
 ```
 
-gluumy is a strongly-typed Forth-inspired language that sits atop any Lua
-compatible with 5.1 or newer, supporting ahead-of-time compilation as well as
-interpretation (perhaps at a REPL). What it lacks in academic background it
-tries to make up for in simplicity, extreme flexibility, and a general Get Shit
-Done vibe.
-
-> Tangential: gluumy's fairly-minimalist type system, `cheereo` is implemented
-> in a single file of pure Lua, and can be used as at least a _runtime_ type
-> system for Lua, MoonScript, Fennel, and other languages targeting Lua. The
-> truly adventurous can probably figure out how to use cheereo for _static_
-> type checking in those languages similarly to how gluumy does; such efforts
-> are left as an exercise to those communities.
->
-> For more about cheereo, peruse the `src/cheereo.lua` file, which is heavily
-> commented.
+gluumy is a strongly-typed Forth-inspired language........ (to be continued).
+What it lacks in academic background it tries to make up for in simplicity,
+extreme flexibility, and a general Get Shit Done vibe.
 
 ## Philosophy
 
@@ -42,12 +30,19 @@ opaque monoliths handed down by powers that be.
 gluumy does not exist in a zero-sum vaccuum of languages, and is not the
 correct tool for every job. It sits somewhere approximately in the altitude of
 languages like Python, Ruby, JavaScript, and can even be considered in some
-places where Go or Haskell might be used, but is _not_ an appropriate
+places where Go or Haskell might be used, but is not always an appropriate
 replacement for low-level languages in the domains where such low-level control
 is necessary. It's expected, for example, that a functional gluumy stack is
-likely made up of C, Zig, and/or Rust componentry, and some gluumy "libraries"
-may be best implemented as FFI wrappers around Lua bindings to such foreign
-componentry.
+likely made up of C, Zig, and/or Rust componentry and FFI bindings thereto.
+
+> Please note that gluumy is a personal side project, mostly aimed towards
+> developing things I want to build (which generally means command line and/or
+> networked applications, and glue scripts). The standard library is thus only
+> as complete as is necessary to solve those problems (and, of course, to
+> self-host the toolchain). If you find this language interesting and choose to
+> use it, be prepared to have to fill in holes in the standard library and/or
+> to have to write FFI bindings and most of all, don't expect API stability
+> across versions yet.
 
 ### On Modules and Package Management
 
@@ -72,11 +67,9 @@ allows for a few cool features and workflows:
 > understood as well as your own code, not treated as a foreign black box of
 > magic!
 
-gluumy does not, and does not currently plan to, have its own package manager.
-Somewhere probably just shy of a billion of these things have already been
-written, and some of them are quite good. If you need to pull external
-dependencies into your project, consider any of the following options to
-retrieve and version them:
+gluumy does not currently have its own package manager. If you need to pull
+external dependencies into your project, consider any of the following options
+to retrieve and version them:
 
 - [Nix](https://nixos.org/manual/nix/stable/)
 - [Guix](https://guix.gnu.org/)
@@ -91,51 +84,6 @@ retrieve and version them:
 For more about the aforementioned config file for configuring search paths and
 resolving function conflicts, see `man 5 gluumy.conf` (link TBD, and will also
 become part of `gluumy doc` eventually).
-
-## This Repo
-
-This repository contains various components:
-
-- `src/stage1` contains the AOT-only bootstrapping compiler, implemented in
-  Lua. It is **only** supported for the purpose of bootstrapping the gluumy
-  toolchain which is, itself, written in gluumy. This compiler can run anywhere
-  Lua 5.1 or newer (thus including LuaJIT) can, and bundles all of its runtime
-  and test-time dependencies in pure Lua (however, if a native
-  [LPeg](http://www.inf.puc-rio.br/~roberto/lpeg/) library is available, it
-  will be used instead of the bundled [LuLPeg](https://github.com/pygy/LuLPeg)).
-
-- `lib/compile`, `lib/tc`, `lib/lsp`, `lib/lint`, and `lib/fmt` are the
-  actually-safe and as-production-ready-as-feasible gluumy compiler,
-  type-checking engine, [language server](https://langserver.org/), linter, and
-  formatter. They are all implemented in gluumy.
-
-- `lib/core` and `lib/std` define the base language and standard library, each
-  also implemented in gluumy.
-
-> Please note that gluumy is a personal side project, mostly aimed towards
-> developing things I want to build (which generally means command line and/or
-> networked applications, and glue scripts). The standard library is thus only
-> as complete as is necessary to solve those problems (and, of course, to
-> self-host the toolchain). If you find this language interesting and choose to
-> use it, be prepared to have to fill in holes in the standard library and/or
-> to have to write FFI bindings and typedefs to Lua modules, and most of all,
-> don't expect API stability across versions yet.
-
-## Dependencies
-
-- Any Lua compatible with the intersection of the [LuaJIT-defined subset of Lua
-  5.2](https://luajit.org/extensions.html) and
-  [lua-compat-5.2](https://github.com/keplerproject/lua-compat-5.2/). In
-  practical terms, on most Unixes this means LuaJIT, Lua 5.2, Lua 5.1 with
-  `compat52`, or anything else backwards-compatible to those APIs. Clear as
-  mud, thanks Lua fragmentation!
-
-> Note: currently, the standard library is basic enough that it'll probably run
-> anywhere something resembling PUC Lua can run. It's possible that future
-> revisions of gluumy will take optional dependencies on further libraries to
-> provide, for example, stdlib bindings to a network request library. TBD, but
-> those running gluumy on non-Unix platforms (as well as packagers) should keep
-> an eye on project commits/releases for this reason.
 
 ## Thanks
 
@@ -154,12 +102,7 @@ alphabetical order, here's some of those people:
 released under your choice of either of the following terms. Whichever you
 choose, have fun with it, build cool stuff with it, don't exploit your fellow
 humans or the world at large with it, and generally don't be an ass within or
-outside of the project or anything written with it. Further, while it's not a
-license term (and is instead more of a handshake request), I ask that you
-please find some other name for significant derivatives of gluumy - I'm
-thrilled if you want to target Python or Ruby instead of Lua, but to avoid
-confusing folks, please find some other name for your repo than, for example,
-`gluumy-py`. Maybe `pyluumy`, I dunno.
+outside of the project or anything written with it.
 
 - The [Guthrie Public
   License](https://web.archive.org/web/20180407192134/https://witches.town/@ThatVeryQuinn/3540091)
@@ -168,9 +111,3 @@ confusing folks, please find some other name for your repo than, for example,
 - The [Creative Commons Zero 1.0
   dedication](https://creativecommons.org/publicdomain/zero/1.0/), which is
   public domain or maximally-permissive, as your jurisdiction allows.
-
-This repository redistributes the following third-party code, under their
-original license terms:
-
-- [luaunit](https://github.com/bluebird75/luaunit), a BSD-licensed unit testing
-  library
--- src/main.zig
+++ src/main.zig
@@ -36,6 +36,8 @@
 // With that said, let's begin the "host" side of gluumy.
 
 const std = @import("std");
+const Allocator = std.mem.Allocator;
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 
 // Words in gluumy work somewhat like words in Forth, but with some critically
 // important details largely stemming from gluumy's type system, and its
@@ -58,60 +60,333 @@ const std = @import("std");
 // implementation will scale proportionally.
 const WORD_BUF_LEN = 1024;
 
-const ObjType = struct {};
+const STACK_LENGTH = 4096;
+const TOP_TYPE_ID: TypeSignature.IdType = 0;
 
-const Word = union(enum) {
-    Native: struct {
-        flags: packed struct {
-            immediate: bool,
-            hidden: bool,
-        },
+// TODO: determine if this is temporary, or will actually exist in the final
+// implementation. it would probably make words like dup more sane, but then,
+// type hints like ( @1 -> @1 ) to make "generics" could get us just as good of
+// an effect (I think)
+var TopType = TypeSignature.init(false, TOP_TYPE_ID);
+
+const Dictionary = struct {
+    const Self = @This();
+    const Store = std.StringHashMap(WordImplementationsByTypeSignature);
+
+    allocator: *Allocator,
+    store: *Store,
+
+    fn init(allocator: *Allocator) !Self {
+        var store = try allocator.create(Store);
+        store.* = Store.init(allocator.*);
+
+        return Self{
+            .allocator = allocator,
+            .store = store,
+        };
+    }
+
+    // TODO take type signature into account
+    fn lookup(self: Self, name: []const u8) ?*Word {
+        std.debug.print("looking up \"{s}\"\n", .{name});
+        return self.store.get(name).?.getPtr(&TopType);
+    }
+
+    // TODO take type signature into account
+    fn register(self: Self, name: []u8, word: Word) !void {
+        var name_storage = try self.allocator.alloc(u8, name.len);
+        std.mem.copy(u8, name_storage, name);
+
+        var entry = try self.store.getOrPut(name_storage);
+
+        if (!entry.found_existing) {
+            var impls = try self.allocator.create(WordImplementationsByTypeSignature);
+            impls.* = WordImplementationsByTypeSignature.init(self.allocator.*);
+            entry.value_ptr = impls;
+        }
+
+        _ = try entry.value_ptr.getOrPutValue(&TopType, word);
+
+        std.debug.print("stored word \"{s}\" of type {any} with {any}\n", .{
+            name_storage,
+            TopType,
+            word,
+        });
+    }
+
+    comptime {
+        std.testing.refAllDecls(@This());
+    }
+};
+
+const WordImplementationsByTypeSignature = std.AutoHashMap(*TypeSignature, Word);
+
+const TypeSignature = packed struct {
+    const Self = @This();
+
+    const IdType = u20;
+    const PrimaryPaddingSize = u11;
+
+    reserved_padding: PrimaryPaddingSize,
+    is_container: bool,
+    id: IdType,
+
+    fn init(is_container: bool, id: IdType) Self {
+        return Self{
+            .reserved_padding = 0,
+            .is_container = is_container,
+            .id = id,
+        };
+    }
+
+    test {
+        try std.testing.expectEqual(32, @bitSizeOf(@This()));
+    }
+
+    comptime {
+        std.testing.refAllDecls(@This());
+    }
+};
+
+const Word = struct {
+    const Self = @This();
+
+    flags: packed struct {
+        immediate: bool,
+        hidden: bool,
     },
+    implementation: WordImplementation,
+
+    fn initSimplePrimitive(impl: *const WordImplementation.PrimitiveImplementation) Self {
+        return Self{
+            .flags = .{
+                .immediate = false,
+                .hidden = false,
+            },
+            .implementation = WordImplementation{ .Primitive = impl },
+        };
+    }
 
     comptime {
         std.testing.refAllDecls(@This());
     }
 };
 
-const ObjectHeader = struct {
-    obj_type: ObjType,
+const WordImplementation = union(enum) {
+    const PrimitiveImplementation = (fn (Runtime, *Stack) Runtime.Error!*Stack);
+
+    Primitive: *const PrimitiveImplementation,
+    WordSequence: []*Word,
+    Constant: *Object,
 
     comptime {
         std.testing.refAllDecls(@This());
     }
 };
 
-const Object = struct {
-    header: ObjectHeader,
+const Object = union(enum) {
+    const Self = @This();
+
+    // change to suit taste/target platform
+    const StandardFloatType = f64;
+
+    // TODO: figure out how this should work, it's going to be pretty critical
+    // for getting the language off the ground (read: I don't feel like
+    // reimplementing, say, LibreSSL in gluumy)
+    Foreign: *anyopaque,
+
+    UnsignedInt: usize,
+    SignedInt: isize,
+    Float: StandardFloatType,
+
+    fn is_number(self: Self) bool {
+        return switch (self) {
+            .UnsignedInt, .SignedInt, .Float => true,
+            else => false,
+        };
+    }
 
     comptime {
         std.testing.refAllDecls(@This());
     }
 };
 
-const Stack = std.linked_list.TailQueue(Object);
+// TODO: is it possible to not park the pointer to the stack_node within the
+// stack_node.data itself? seems super ugly, but is the only way I could think
+// of to avoid allocating a whole other store for "in use" StackNodePool
+// entities
+//
+// besides, anything that's a pointer to anyopaque seems... overly hacky
+const ObjectWrapper = struct {
+    entity: ?*anyopaque,
+    obj: Object,
+};
+
+const Stack = std.atomic.Stack(?ObjectWrapper);
+const StackNodePool = std.atomic.Stack(*Stack.Node);
 
 const InterpState = enum(u8) {
-    IMMEDIATE = 0,
+    INTERPRET = 0,
     COMPILE = 1,
+    IMMEDIATE = 2,
+};
+
+const Runtime = struct {
+    const Self = @This();
+
+    const Error = error{
+        StackUnderflow,
+        RuntimeTypeError,
+    };
+
+    allocator: *Allocator,
+    stack: Stack,
+    stack_node_pool: *StackNodePool,
+    word_dictionary: Dictionary,
+    state: InterpState,
+    type_id_counter: TypeSignature.IdType,
+
+    fn init(allocator: *Allocator) !Self {
+        var stack_node_pool = try allocator.create(StackNodePool);
+        stack_node_pool.* = StackNodePool.init();
+
+        var stack_nodes = try allocator.alloc(StackNodePool.Node, STACK_LENGTH);
+
+        var runtime = Self{
+            .allocator = allocator,
+            .stack = Stack.init(),
+            .stack_node_pool = stack_node_pool,
+
+            .word_dictionary = try Dictionary.init(allocator),
+
+            .state = InterpState.INTERPRET,
+            .type_id_counter = TOP_TYPE_ID + 1,
+        };
+
+        for (stack_nodes[0..]) |*node_slot| {
+            var node = try runtime.allocator.create(Stack.Node);
+            node.* = Stack.Node{
+                .next = null,
+                .data = null,
+            };
+
+            node_slot.* = StackNodePool.Node{
+                .next = null,
+                .data = node,
+            };
+            runtime.stack_node_pool.push(node_slot);
+        }
+
+        return runtime;
+    }
+
+    fn populate_primitve_words(self: *Self) !void {
+        // TODO optimization:
+        // self.word_dictionary.store.ensureTotalCapacity(X);
+        //
+        // where X is the number of primitives we're registering, perhaps store
+        // in an anonymous slice
+
+        var star_word = "*".*;
+        try self.word_dictionary.register(
+            star_word[0..],
+            Word.initSimplePrimitive(&prim_word_mul),
+        );
+    }
+
+    fn prim_word_mul(self: Self, stack: *Stack) Error!*Stack {
+        const right = stack.pop().?.data;
+        const left = stack.pop().?.data;
+
+        if (right == null or left == null) {
+            return Error.StackUnderflow;
+        }
+
+        // TODO return old nodes to pool
+        //
+        // TODO clean up old Objects, they currently just "leak" by way of
+        // using the ArenaAllocator (read: use the GPA instead)
+        try switch (right.?.obj) {
+            Object.Float => |rval| switch (left.?.obj) {
+                Object.Float => |lval| {
+                    const result = rval * lval;
+                    var stack_node = self.stack_node_pool.pop().?;
+                    stack_node.data.data.?.obj = Object{ .Float = result };
+                    stack_node.data.data.?.entity = stack_node;
+                    stack.push(stack_node.data);
+                },
+                else => Error.RuntimeTypeError,
+            },
+            Object.UnsignedInt => |rval| switch (left.?.obj) {
+                Object.UnsignedInt => |lval| {
+                    const result = rval * lval;
+                    var stack_node = self.stack_node_pool.pop().?;
+                    stack_node.data.data.?.obj = Object{ .UnsignedInt = result };
+                    stack_node.data.data.?.entity = stack_node;
+                    stack.push(stack_node.data);
+                },
+                else => Error.RuntimeTypeError,
+            },
+            Object.SignedInt => |rval| switch (left.?.obj) {
+                Object.SignedInt => |lval| {
+                    const result = rval * lval;
+                    var stack_node = self.stack_node_pool.pop().?;
+                    stack_node.data.data.?.obj = Object{ .SignedInt = result };
+                    stack_node.data.data.?.entity = stack_node;
+                    stack.push(stack_node.data);
+                },
+                else => Error.RuntimeTypeError,
+            },
+            else => Error.RuntimeTypeError,
+        };
+
+        return stack;
+    }
+
+    fn feed_word_TEMP(self: Self, word: []const u8) !void {
+        // TODO integrate type system, everything is currently assumed
+        // TOP_TYPE_ID since, well, duh, there's no type system yet
+        const found_word = self.word_dictionary.lookup(word).?;
+
+        // TODO implement
+        if (found_word.flags.immediate) unreachable;
+        if (found_word.flags.hidden) unreachable;
+    }
+
+    comptime {
+        std.testing.refAllDecls(Self);
+    }
 };
 
 pub fn main() anyerror!u8 {
-    const stdin = std.io.getStdIn();
-    const stdin_reader = stdin.reader();
-    const stdout = std.io.getStdOut();
-    const stdout_writer = stdout.writer();
+    var gpa_allocator = gpa.allocator();
+    var arena_alloc = std.heap.ArenaAllocator.init(gpa_allocator);
+    var arena_allocator = arena_alloc.allocator();
+
+    // TODO: fix this
+    var runtime = try Runtime.init(&arena_allocator);
+
+    try runtime.populate_primitve_words();
+
+    const raw_stdin = std.io.getStdIn();
+    //var stdin = std.io.bufferedReader(raw_stdin.reader());
+    //var stdin_reader = stdin.reader();
+    var stdin_reader = raw_stdin.reader();
+    const raw_stdout = std.io.getStdOut();
+    var stdout = std.io.bufferedWriter(raw_stdout.writer());
+    var stdout_writer = stdout.writer();
 
     var word_buf: [WORD_BUF_LEN]u8 = undefined;
     var word_len: usize = 0;
+
+    // park this var to appease compiler
+    _ = stdout_writer;
 
     while (true) {
         const word_byte = stdin_reader.readByte() catch |err| switch (err) {
             error.EndOfStream => {
                 if (word_len > 0) {
-                    try stdout_writer.writeAll("word found eof: ");
-                    try stdout_writer.writeAll(word_buf[0..word_len]);
-                    try stdout_writer.writeByte('\n');
+                    try runtime.feed_word_TEMP(word_buf[0..word_len]);
                 }
 
                 break;
@@ -124,9 +399,7 @@ pub fn main() anyerror!u8 {
 
         switch (word_byte) {
             ' ', '\t', '\n' => {
-                try stdout_writer.writeAll("word found: ");
-                try stdout_writer.writeAll(word_buf[0..word_len]);
-                try stdout_writer.writeByte('\n');
+                try runtime.feed_word_TEMP(word_buf[0 .. word_len - 1]);
                 word_len = 0;
                 continue;
             },
@@ -134,13 +407,11 @@ pub fn main() anyerror!u8 {
         }
     }
 
+    try stdout.flush();
+
     return 0;
 }
 
 comptime {
     std.testing.refAllDecls(@This());
-}
-
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
 }
