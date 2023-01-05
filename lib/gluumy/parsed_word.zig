c49acec6226a086efa3238561bad35d6e423988b
Josh Klar <josh@klar.sh>
2023-01-05 02:36:42 -0800

    parsed_word: style cleanup, add some missing assertions

--- lib/gluumy/parsed_word.zig
+++ lib/gluumy/parsed_word.zig
@@ -3,6 +3,11 @@
 // COPYING.
 
 const std = @import("std");
+const expect = std.testing.expect;
+const expectApproxEqAbs = std.testing.expectApproxEqAbs;
+const expectEqual = std.testing.expectEqual;
+const expectEqualStrings = std.testing.expectEqualStrings;
+const expectError = std.testing.expectError;
 
 const InternalError = @import("./internal_error.zig").InternalError;
 const helpers = @import("./helpers.zig");
@@ -92,6 +97,8 @@ const SYMBOL_WORD_DELIMITER = helpers.CHAR_QUOTE_SGL;
 const REF_WORD_DELIMITER = helpers.CHAR_AMPER;
 
 pub const ParsedWord = union(enum) {
+    const Self = @This();
+
     pub const SimpleWordReference = struct {
         name: []const u8,
         semantics: packed struct {
@@ -118,10 +125,7 @@ pub const ParsedWord = union(enum) {
     /// - Floats
     /// - Ints
     /// - Assumed actual words ("Simples")
-    //
-    // TODO: symbols should be interned somewhere for memory savings. How,
-    // exactly, to do this, is left as an exercise to my future self.
-    pub fn from_input(input: []const u8) !@This() {
+    pub fn from_input(input: []const u8) !Self {
         if (input.len == 0 or std.mem.eql(u8, EMPTY_STRING, input)) {
             return InternalError.EmptyWord;
         }
@@ -129,7 +133,9 @@ pub const ParsedWord = union(enum) {
         // TODO: This presumes that string quote handling actually happens a
         // level above (read: that the word splitter understands that "these
         // are all one word"), which probably isn't the cleanest design
-        if (input[0] == STRING_WORD_DELIMITER and input[input.len - 1] == STRING_WORD_DELIMITER) {
+        if ((input[0] == STRING_WORD_DELIMITER) and
+            (input[input.len - 1] == STRING_WORD_DELIMITER))
+        {
             return ParsedWord{ .String = input[1 .. input.len - 1] };
         }
 
@@ -205,7 +211,7 @@ pub const ParsedWord = union(enum) {
     }
 
     test "errors on empty words" {
-        try std.testing.expectError(InternalError.EmptyWord, from_input(EMPTY_STRING));
+        try expectError(InternalError.EmptyWord, from_input(EMPTY_STRING));
     }
 
     test "parses strings: basic" {
@@ -213,68 +219,44 @@ pub const ParsedWord = union(enum) {
             "\"I don't know me and you don't know you\"",
         )).String;
 
-        try std.testing.expectEqualStrings(
+        try expectEqualStrings(
             "I don't know me and you don't know you",
             result,
         );
     }
 
     test "parses strings: unicodey" {
-        const result = (try from_input(
-            "\"yeee 🐸☕ hawwww\"",
-        )).String;
-
-        try std.testing.expectEqualStrings(
-            "yeee 🐸☕ hawwww",
-            result,
-        );
+        const result = (try from_input("\"yeee 🐸☕ hawwww\"")).String;
+        try expectEqualStrings("yeee 🐸☕ hawwww", result);
     }
 
     test "parses symbols: basic" {
-        const result = (try from_input(
-            "'Testable",
-        )).Symbol;
-
-        try std.testing.expectEqualStrings(
-            "Testable",
-            result,
-        );
+        const result = (try from_input("'Testable")).Symbol;
+        try expectEqualStrings("Testable", result);
     }
 
     test "parses symbols: unicodey" {
-        const result = (try from_input(
-            "'🐸☕",
-        )).Symbol;
-
-        try std.testing.expectEqualStrings(
-            "🐸☕",
-            result,
-        );
+        const result = (try from_input("'🐸☕")).Symbol;
+        try expectEqualStrings("🐸☕", result);
     }
 
     test "parses refs: basic" {
-        const result = (try from_input(
-            "&Testable",
-        )).Ref;
-
-        try std.testing.expectEqualStrings(
-            "Testable",
-            result,
-        );
+        const result = (try from_input("&Testable")).Ref;
+        try expectEqualStrings("Testable", result);
     }
 
     test "parses floats: bare" {
-        try std.testing.expectApproxEqAbs(
+        try expectApproxEqAbs(
             @as(f64, 3.14),
             (try from_input("3.14")).NumFloat,
             @as(f64, 0.001),
         );
-        try std.testing.expectApproxEqAbs(
+        try expectApproxEqAbs(
             @as(f64, 0.0),
             (try from_input("0.0")).NumFloat,
             @as(f64, 0.0000001),
         );
-        try std.testing.expectApproxEqAbs(
+        try expectApproxEqAbs(
             @as(f64, 0.0),
             (try from_input("0.000000000000000")).NumFloat,
             @as(f64, 0.0000001),
@@ -282,69 +264,57 @@ pub const ParsedWord = union(enum) {
     }
 
     test "parses ints: bare" {
-        try std.testing.expectEqual(
-            @as(isize, 420),
-            (try from_input("420")).SignedInt,
-        );
-        try std.testing.expectEqual(
-            @as(isize, -1337),
-            (try from_input("-1337")).SignedInt,
-        );
+        try expectEqual(@as(isize, 420), (try from_input("420")).SignedInt);
+        try expectEqual(@as(isize, -1337), (try from_input("-1337")).SignedInt);
     }
 
     test "parses ints: suffixes" {
-        try std.testing.expectEqual(
-            @as(isize, 420),
-            (try from_input("420/i")).SignedInt,
-        );
-
-        try std.testing.expectEqual(
-            @as(usize, 420),
-            (try from_input("420/u")).UnsignedInt,
-        );
+        try expectEqual(@as(isize, 420), (try from_input("420/i")).SignedInt);
+        try expectEqual(@as(usize, 420), (try from_input("420/u")).UnsignedInt);
     }
 
     test "parses ints: unhandled suffixes" {
-        try std.testing.expectError(
-            InternalError.UnknownSlashedSuffix,
-            from_input("12345/z"),
-        );
+        try expectError(InternalError.UnknownSlashedSuffix, from_input("12345/z"));
     }
 
     test "parses simple word incantations" {
         const result = (try from_input("@BEFORE_WORD")).Simple;
-        try std.testing.expectEqualStrings("@BEFORE_WORD", result.name);
+        try expectEqualStrings("@BEFORE_WORD", result.name);
+        try expect(result.semantics.stash_before_lookup == false);
+        try expect(result.semantics.hoist_after_result == false);
     }
 
     test "parses word names: stashing mode" {
         const result = (try from_input(",@BEFORE_WORD")).Simple;
-        try std.testing.expectEqualStrings("@BEFORE_WORD", result.name);
-        try std.testing.expect(result.semantics.stash_before_lookup);
+        try expectEqualStrings("@BEFORE_WORD", result.name);
+        try expect(result.semantics.stash_before_lookup);
+        try expect(result.semantics.hoist_after_result == false);
     }
 
     test "parses word names: hoisting mode" {
         const result = (try from_input("@BEFORE_WORD,")).Simple;
-        try std.testing.expectEqualStrings("@BEFORE_WORD", result.name);
-        try std.testing.expect(result.semantics.hoist_after_result);
+        try expectEqualStrings("@BEFORE_WORD", result.name);
+        try expect(result.semantics.stash_before_lookup == false);
+        try expect(result.semantics.hoist_after_result);
     }
 
     test "parses word names: stashing+hoisting mode" {
         const result = (try from_input(",@BEFORE_WORD,")).Simple;
-        try std.testing.expectEqualStrings("@BEFORE_WORD", result.name);
-        try std.testing.expect(result.semantics.stash_before_lookup);
-        try std.testing.expect(result.semantics.hoist_after_result);
+        try expectEqualStrings("@BEFORE_WORD", result.name);
+        try expect(result.semantics.stash_before_lookup);
+        try expect(result.semantics.hoist_after_result);
     }
 
     test "word names must not contain internal commas" {
-        try std.testing.expectError(InternalError.InvalidWordName, from_input(","));
-        try std.testing.expectError(InternalError.InvalidWordName, from_input(",,"));
-        try std.testing.expectError(InternalError.InvalidWordName, from_input(",,,"));
-        try std.testing.expectError(InternalError.InvalidWordName, from_input("foo,bar"));
-        try std.testing.expectError(InternalError.InvalidWordName, from_input(",,foo"));
-        try std.testing.expectError(InternalError.InvalidWordName, from_input(",,foo,"));
-        try std.testing.expectError(InternalError.InvalidWordName, from_input(",,foo,,"));
-        try std.testing.expectError(InternalError.InvalidWordName, from_input(",foo,,"));
-        try std.testing.expectError(InternalError.InvalidWordName, from_input("foo,,"));
+        try expectError(InternalError.InvalidWordName, from_input(","));
+        try expectError(InternalError.InvalidWordName, from_input(",,"));
+        try expectError(InternalError.InvalidWordName, from_input(",,,"));
+        try expectError(InternalError.InvalidWordName, from_input("foo,bar"));
+        try expectError(InternalError.InvalidWordName, from_input(",,foo"));
+        try expectError(InternalError.InvalidWordName, from_input(",,foo,"));
+        try expectError(InternalError.InvalidWordName, from_input(",,foo,,"));
+        try expectError(InternalError.InvalidWordName, from_input(",foo,,"));
+        try expectError(InternalError.InvalidWordName, from_input("foo,,"));
     }
 };
 
